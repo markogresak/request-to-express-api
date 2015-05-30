a579ef9c314e3f8f2d9f394e1f5c56e0f156b695
Marko Grešak <marko@gresak.io>
2015-05-30 05:27:36 +0200

    make a request to server/api with exampleData, log response

---
+++ client.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var request = require('request');
+
+var exampleData = {
+  number: 10,
+  string: 'some string',
+  date: new Date(),
+  array: [1, 2, 3],
+  object: {
+    x: 10,
+    y: 5
+  }
+};
+
+request.get({
+  url: 'http://localhost:3000/api',
+  data: exampleData
+}, function (err, httpResponse, body) {
+  console.log('response data:', body);
+});
