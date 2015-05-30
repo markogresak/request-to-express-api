3fbb9b85a1944267e91ccbef9f812658e1c4c25a
Marko Grešak <marko@gresak.io>
2015-05-30 05:26:22 +0200

    add /api router, logs and returns request data field

--- server.js
+++ server.js
@@ -10,6 +10,14 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(bodyParser.json());
 
+// Init express router on /api.
+var router = express.Router();
+router.get('/api', function (req, res) {
+  // Get `data` field from request, log it and return it as json response.
+  console.log('received data:', req.body.data);
+  res.json(req.body.data);
+});
+app.use('/', router);
 
 // Start server on port 3000.
 app.listen(3000);
